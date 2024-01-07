cf52ab4fa35a1aad1b02287c5575ffb47f009e8e
Brett Tolbert <bretttolbert@gmail.com>
2024-01-07 13:27:18 -0600

    type hints and check for trailing space

--- mediatest.py
+++ mediatest.py
@@ -1,49 +1,51 @@
 # !/usr/bin/python3
 import os
-from pathlib import Path
-import pytest
 import re
 
-MUSIC_DIR="/data/Music/"
-EXPECTED_MEDIA_COUNT=11549
+MUSIC_DIR = "/data/Music/"
+EXPECTED_MEDIA_COUNT = 12261
 
 # intentionally lowercase for consistency, ".JPG" not allowed, etc.
-EXTS_MEDIA = ['mp3', 'm4a']
-EXTS_ART = ['jpg', 'webp', 'png']
-EXTS_EXTRA = ['pdf']
+EXTS_MEDIA = ["mp3", "m4a"]
+EXTS_ART = ["jpg", "webp", "png"]
+EXTS_EXTRA = ["pdf"]
 ALLOWED_EXTS = EXTS_MEDIA + EXTS_ART + EXTS_EXTRA
 
-def get_file_ext(path):
-    root, ext = os.path.splitext(path)
-    return ext.strip('.')
+
+def get_file_ext(path: str) -> str:
+    _, ext = os.path.splitext(path)
+    return ext.strip(".")
+
 
 def test_allowed_exts():
     count = 0
-    for root, dirs, files in os.walk(MUSIC_DIR, topdown=False):
+    for _, _, files in os.walk(MUSIC_DIR, topdown=False):
         for name in files:
             ext = get_file_ext(name)
-            assert ext in ALLOWED_EXTS, '{} not in allowed extensions'.format(name)
+            assert ext in ALLOWED_EXTS, "{} not in allowed extensions".format(name)
     print(count)
+
 
 def test_media_file_count():
     count_media = 0
     count_mp3 = 0
     count_m4a = 1
-    for root, dirs, files in os.walk(MUSIC_DIR, topdown=False):
+    for _, _, files in os.walk(MUSIC_DIR, topdown=False):
         for name in files:
             ext = get_file_ext(name)
             if ext in EXTS_MEDIA:
                 count_media += 1
-            if ext == 'mp3':
+            if ext == "mp3":
                 count_mp3 += 1
-            elif ext == 'm4a':
+            elif ext == "m4a":
                 count_m4a += 1
-    print('count_mp3={}'.format(count_mp3))
-    print('count_m4a={}'.format(count_m4a))
-    print('count_media={}'.format(count_media))
+    print("count_mp3={}".format(count_mp3))
+    print("count_m4a={}".format(count_m4a))
+    print("count_media={}".format(count_media))
     assert count_media == EXPECTED_MEDIA_COUNT
 
-def assert_directory_contains_media(path):
+
+def assert_directory_contains_media(path: str):
     """Asserts that directory contains at least one media file
     and that directory does not contain subdirectories"""
     ret = False
@@ -54,7 +56,8 @@ def assert_directory_contains_media(path):
             ret = True
     assert ret
 
-def assert_directory_contains_subdirectories(path):
+
+def assert_directory_contains_subdirectories(path: str):
     """Asserts that directory contains at least one subdirectory
     and that directory does not contain any files"""
     ret = False
@@ -65,13 +68,15 @@ def assert_directory_contains_subdirectories(path):
             ret = True
     assert ret
 
-def directory_contains_cover_jpg(path):
-    """Asserts that directory contains cover.jpg"""
-    return 'cover.jpg' in os.listdir(path)
+
+def directory_contains_cover_jpg(path: str) -> bool:
+    """Returns whether directory contains cover.jpg or not"""
+    return "cover.jpg" in os.listdir(path)
+
 
 def test_no_empty_dirs():
     baselevel = len(MUSIC_DIR.split(os.path.sep))
-    for root, dirs, files in os.walk(MUSIC_DIR, topdown=False):
+    for root, dirs, _ in os.walk(MUSIC_DIR, topdown=False):
         for name in dirs:
             fullpath = os.path.join(root, name)
             curlevel = len(fullpath.split(os.path.sep))
@@ -85,7 +90,14 @@ def test_no_empty_dirs():
             else:
                 assert False, "folder nested too deep: {}".format(fullpath)
 
-#@pytest.mark.skip(reason="wip")
+
+def string_contains_trailing_space(s: str) -> bool:
+    """Trailing space can causes problems e.g. when burning to a BluRay on Windows,
+    I was getting 'file not found' errors for some albums that had trailing space"""
+    return bool(s != s.strip())
+
+
+# @pytest.mark.skip(reason="wip")
 def test_album_dir_name():
     # prohibit chars not allowed in windows filenames
     # and other problematic characters
@@ -93,7 +105,7 @@ def test_album_dir_name():
     baselevel = len(MUSIC_DIR.split(os.path.sep))
     count = 0
     match_count = 0
-    for root, dirs, files in os.walk(MUSIC_DIR, topdown=False):
+    for root, dirs, _ in os.walk(MUSIC_DIR, topdown=False):
         for name in dirs:
             fullpath = os.path.join(root, name)
             curlevel = len(fullpath.split(os.path.sep))
@@ -101,23 +113,33 @@ def test_album_dir_name():
             # 1 = album [year]
             if curlevel - baselevel == 1:
                 count += 1
-                if album_pattern.match(name):
+                if album_pattern.match(name) and not string_contains_trailing_space(
+                    name
+                ):
                     match_count += 1
                 else:
-                    print('bad album dir name format: {}'.format(fullpath))
-    print("test_dir_name: bad album dir format: {} out of {}".format(count - match_count, count))
-    print("test_dir_name: good album dir format: {} out of {}".format(match_count, count))
-    assert count == match_count, 'one or more bad album formats'
+                    print("bad album dir name format: {}".format(fullpath))
+    print(
+        "test_dir_name: bad album dir format: {} out of {}".format(
+            count - match_count, count
+        )
+    )
+    print(
+        "test_dir_name: good album dir format: {} out of {}".format(match_count, count)
+    )
+    assert count == match_count, "One or more invalid album names"
+
 
 # TODO: Write test to disallow periods in filenames except as file extension separator
 # TODO: Write test to find duplicated artists, e.g. "The Dave Matthews Band" vs "Dave Matthews Band"
 # TODO: Validate filenames, prohibited chars in filenames
 
+
 def test_album_cover_jpg():
     baselevel = len(MUSIC_DIR.split(os.path.sep))
     count = 0
     match_count = 0
-    for root, dirs, files in os.walk(MUSIC_DIR, topdown=False):
+    for root, dirs, _ in os.walk(MUSIC_DIR, topdown=False):
         for name in dirs:
             fullpath = os.path.join(root, name)
             curlevel = len(fullpath.split(os.path.sep))
@@ -128,7 +150,15 @@ def test_album_cover_jpg():
                 if directory_contains_cover_jpg(fullpath):
                     match_count += 1
                 else:
-                    print('missing cover.jpg dir: {}'.format(fullpath))
-    print("test_album_cover_jpg: missing cover.jpg dir: {} out of {}".format(count - match_count, count))
-    print("test_album_cover_jpg: has cover.jpg dir: {} out of {}".format(match_count, count))
-    assert count == match_count, 'one or more album dirs missing cover.jpg'
+                    print("Missing cover.jpg dir: {}".format(fullpath))
+    print(
+        "test_album_cover_jpg: Missing cover.jpg dir: {} out of {}".format(
+            count - match_count, count
+        )
+    )
+    print(
+        "test_album_cover_jpg: has cover.jpg dir: {} out of {}".format(
+            match_count, count
+        )
+    )
+    assert count == match_count, "One or more album dirs missing cover.jpg"
